35b29306810e2f860f44ad42faf359b0da59f941
Greta <greta.maria.brugnatti.2007@calvino.edu.it>
2025-10-12 14:59:38 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<html>
+    <head>
+        <title> Registrazione - Biblioteca </title>
+        <style>
+            form{
+                display: flex;
+                flex-direction: column;
+                width: 10%;
+            }
+        </style>
+    </head>
+    <body>
+        <form action="index.php" method="POST">
+
+            <!-- <label>Username: </label>
+            <input type="text" name="username"> -->
+            <label> Nome: </label>
+            <input type="text" name="nome">
+            <label> Cognome: </label>
+            <input type="text" name="cognome">
+            <label> Data di nascita: </label>
+            <input type="date" name="data_nascita">
+            <label> Luogo di nascita: </label>
+            <input type="text" name="luogo_nascita">
+            <label> Codice fiscale: </label>
+            <input type="text" name="codice_fiscale">
+            <label> Numero di telefono: </label>
+            <input type="text" name="numero_telefono">
+
+            <input type="submit">
+
+        </form>
+    </body>
+    <?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "i5ai3";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if($conn->connect_error){
+            die("Connessione fallita: " + $conn->connect_error);
+        } else {
+            echo("Connesso al server<br>");
+        }
+
+        $nome = $_POST["nome"];
+        $cognome = $_POST["cognome"];
+        $data_nascita = $_POST["data_nascita"];
+        $luogo_nascita = $_POST["luogo_nascita"];
+        $codice_fiscale = $_POST["codice_fiscale"];
+        $numero_telefono = $_POST["numero_telefono"];
+
+        
+        /*$username = $_POST["username"];
+        $data_creazione = date('Y-m-d');*/
+
+        $query = "INSERT INTO persona (nome, cognome, data_nascita, luogo_nascita, codice_fiscale, numero_telefono) VALUES ('$nome' , '$cognome', '$data_nascita' , '$luogo_nascita' , '$codice_fiscale' , '$numero_telefono')";
+        echo $query;
+        if (mysqli_query($conn, $query)) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $query . "<br>" . mysqli_error($conn);
+        }
+
+        echo("<br>");
+
+        $query = "SELECT * FROM utente";
+        
+
+        mysqli_close($conn);
+
+    ?>
+</html>
